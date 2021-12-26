456213077b94471f660e93bd03d4a24626455a10
F.AlSaleh <94alsaleh@gmail.com>
2021-12-26 22:42:05 +0300

    Donegit status

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+const BMI = weight / (Math.pow (height/100,2))
+
+alert(`BMI is ${BMI}`)
+
+if(BMI < 18.5 && age == 19 || age <=24) {
+  alert ("Underweight")
+}
+else if(BMI <= 24.5 && age == 25){
+  alert ("Healthy Weight")
+}
+else if(BMI <= 29.9 && age == 19){
+  alert ("Overweight")
+}
+else if(BMI >= 30 && age == 19){
+  alert ("Obesity")
+}
 }
